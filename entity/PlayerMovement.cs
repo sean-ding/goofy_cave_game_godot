72c203dd60e52cbdb218fc9812795a20d7a2ab37
sean-ding <sdwdry150@gmail.com>
2024-01-24 23:03:21 -0500

    yippee

---
+++ entity/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using Godot;
+
+namespace goofy_cave_game_godot.entity;
+
+public partial class PlayerMovement : Node2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
+	}
+}
