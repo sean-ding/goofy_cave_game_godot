d54c6360e284d81369875401714c015cb983f9b8
sean-ding <sdwdry150@gmail.com>
2024-01-26 01:12:37 -0500

    AHHHHHHHHHHHHHH

--- entity/PlayerMovement.cs
+++ entity/PlayerMovement.cs
@@ -1,45 +1,36 @@
 using Godot;
+using System;
 
-namespace goofy_cave_game_godot.entity;
-
-public partial class PlayerMovement : Node2D
+public partial class PlayerMovement : CharacterBody2D
 {
 	[Export]
-	public int Speed { get; set; } = 400;
+	public NodePath SpritePath; 
 	
-	// Called when the node enters the scene tree for the first time.
+	public const float Speed = 30.0f;
+	
+	private Transform2D _lastTransform;
+	private Transform2D _currentTransform;
+
 	public override void _Ready()
 	{
+		
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
+		var velocity = Velocity;
 
-		if (Input.IsActionPressed("move_right"))
+		var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		if (direction != Vector2.Zero)
 		{
-			velocity.X += 1;
+			velocity = direction;
+		}
+		else
+		{
+			velocity = Vector2.Zero;
 		}
 
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
-
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-		}
+		Velocity = velocity;
+		MoveAndCollide(velocity);
 	}
 }
